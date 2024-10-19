2fee61e5774c432c9c30315a2afca1f4ab4293dd
= <syversonshea@gmail.com>
2024-10-18 23:41:42 -0700

    Cleaning.

--- cudaApplication/src/cuda_rt.cu
+++ cudaApplication/src/cuda_rt.cu
@@ -15,7 +15,6 @@ __global__ void d_raytrace(
     int idx = y * width + x;
     Eigen::Vector3d origin = ray_origins[idx];
     Eigen::Vector3d direction = ray_directions[idx];
-    //Perform RT
     // Phong shading parameters
     double diffuse_intensity = 0.4;
     double specular_intensity = 0.4;
@@ -23,15 +22,11 @@ __global__ void d_raytrace(
     double shine = 32.0;
     double a = 1.0, b = 0.1, c = 0.01;
     double brightness = 0.0;
-    
     int max_depth = 3; //Max reflections
-    for (int depth = 0; depth < max_depth; depth++) { //For each reflection
+    for (int depth = 0; depth < max_depth; depth++) {//Perform RT up to depth times for each reflection
         double local_brightness = 0.005, min_t = INF;
         int mindex = find_closest_triangle(origin, direction, nodes, root_index, triangles, min_t);
-        if (mindex == -1) {
-            brightness += 0.0;
-            break;
-        }
+        if (mindex == -1) {brightness += 0.0;break;}
         // Compute intersection point and normal
         Eigen::Vector3d p = origin + direction * min_t;
         Triangle closest = triangles[mindex];
@@ -65,7 +60,6 @@ __global__ void d_raytrace(
         direction.normalize();
         origin = p + direction * 1e-4;
     }
-
     output[idx] = brightness;
 }
 
@@ -79,17 +73,15 @@ double* h_raytrace(
 ) {
     int size = width * height;
     int num_lights = static_cast<int>(light_positions.size());
-
     std::vector<Triangle> triangles = get_triangles(meshes);
     int num_triangles = static_cast<int>(triangles.size());
-
+    // Build BVH
     BvhTree bvh(triangles);
     std::vector<BvhTree::Node>& nodes = bvh.nodes;
     int tree_size = static_cast<int>(nodes.size());
     int root = bvh.root;
-
     double* h_output = new double[size];
-
+    //Device pointers
     Eigen::Vector3d* d_ray_origins = nullptr;
     Eigen::Vector3d* d_ray_directions = nullptr;
     Triangle* d_triangles = nullptr;
@@ -97,7 +89,7 @@ double* h_raytrace(
     double* d_output = nullptr;
     Eigen::Vector3d* d_lights = nullptr;
     Eigen::Vector4d* d_light_colors = nullptr;
-
+    //Allocate
     cudaMalloc((void**)&d_ray_origins, size * sizeof(Eigen::Vector3d));
     cudaMalloc((void**)&d_ray_directions, size * sizeof(Eigen::Vector3d));
     cudaMalloc((void**)&d_triangles, num_triangles * sizeof(Triangle));
@@ -105,27 +97,25 @@ double* h_raytrace(
     cudaMalloc((void**)&d_output, size * sizeof(double));
     cudaMalloc((void**)&d_lights, num_lights * sizeof(Eigen::Vector3d));
     cudaMalloc((void**)&d_light_colors, num_lights * sizeof(Eigen::Vector4d));
-
+    //Copy
     cudaMemcpy(d_ray_origins, ray_origins.data(), size * sizeof(Eigen::Vector3d), cudaMemcpyHostToDevice);
     cudaMemcpy(d_ray_directions, ray_directions.data(), size * sizeof(Eigen::Vector3d), cudaMemcpyHostToDevice);
     cudaMemcpy(d_triangles, triangles.data(), num_triangles * sizeof(Triangle), cudaMemcpyHostToDevice);
     cudaMemcpy(d_nodes, nodes.data(), tree_size * sizeof(BvhTree::Node), cudaMemcpyHostToDevice);
     cudaMemcpy(d_lights, light_positions.data(), num_lights * sizeof(Eigen::Vector3d), cudaMemcpyHostToDevice);
     cudaMemcpy(d_light_colors, light_colors.data(), num_lights * sizeof(Eigen::Vector4d), cudaMemcpyHostToDevice);
-
+    //Kernel
     dim3 blockDim(16, 16);
     dim3 gridDim((width + blockDim.x - 1) / blockDim.x, (height + blockDim.y - 1) / blockDim.y);
-
     d_raytrace<<<gridDim, blockDim>>>(
         d_ray_origins, d_ray_directions, d_nodes, root, d_triangles,
         d_output,
         width, height,
         d_lights, d_light_colors, num_lights
     );
-
+    //Copy back and free
     cudaError_t err = cudaDeviceSynchronize();
     if (err != cudaSuccess) printf("CUDA error. %s\n", cudaGetErrorString(err));
-    
     cudaMemcpy(h_output, d_output, size * sizeof(double), cudaMemcpyDeviceToHost);
     cudaFree(d_ray_origins);
     cudaFree(d_ray_directions);
@@ -134,7 +124,6 @@ double* h_raytrace(
     cudaFree(d_output);
     cudaFree(d_lights);
     cudaFree(d_light_colors);
-
     return h_output;
 }
 
