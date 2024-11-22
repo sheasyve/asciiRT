a49fb0423479fb71aa18945ab567f17dc5e297af
= <syversonshea@gmail.com>
2024-11-22 03:20:59 -0800

    Set up for multiple objects

--- cudaApplication/src/main.cu
+++ cudaApplication/src/main.cu
@@ -1,5 +1,7 @@
 #include "utils/main_util.cuh"
 
+// Set up for 2 teapots
+
 // Scene settings
 int w = 224, h = 224 * 2;
 int padding = 4; // Padding above rendered model
@@ -7,7 +9,7 @@ int padding = 4; // Padding above rendered model
 // Camera settings
 const float focal_length = 2.16;
 const float field_of_view = 0.7854; // 45 degrees
-const V3f camera_position(0, 0, -100); // -100 for car
+const V3f camera_position(0, 0, -50); // -100 for car
 
 // Animation settings
 bool animate = true;
@@ -15,11 +17,12 @@ constexpr int TARGET_FPS = 24;
 constexpr int FRAME_TIME_MS = 1000 / TARGET_FPS;
 
 // Rotation and translation settings
+int mesh_count = 0;
 bool rotate = true;
 bool translate = true;
 std::vector<V3f> rotations;
 std::vector<V3f> translations;
-V3f rotation_increment(0, 0.05, 0);
+std::vector<V3f> rotation_increments;
 
 // Lights
 std::vector<V3f> light_positions;
@@ -31,6 +34,11 @@ std::vector<Mesh> meshes;
 void setup_scene(int argc, char* argv[]){
     load_meshes(argc, argv, meshes); // Load all meshes
     rotations.emplace_back(-0.05, 0.4, 0.05);
+    rotations.emplace_back(-0.05, 0.4, 0.05);
+    translations.emplace_back(0.,0.,0.);
+    translations.emplace_back(6.,-2.,3.);
+    rotation_increments.emplace_back(0., 0.05, 0.);
+    rotation_increments.emplace_back(0., -0.05, 0.);
     // Rotate and translate
     if (!meshes.empty() > 0 && rotate) {
         for (auto r : rotations) meshes[0].triangles = rotate_mesh(meshes[0], r[0], r[1], r[2]);
@@ -53,7 +61,9 @@ void animate_main(){
     while (1) {
         auto start = std::chrono::high_resolution_clock::now();
         if (rotate && !meshes.empty()) {
-            meshes[0].triangles = rotate_mesh(meshes[0], rotation_increment[0], rotation_increment[1], rotation_increment[2]);
+            for (int i = 0; i < mesh_count; i++){
+                meshes[i].triangles = rotate_mesh(meshes[i], rotation_increments[i][0], rotation_increments[i][1], rotation_increments[i][2]);
+            }
         }
         float* output = h_raytrace(meshes, w, h, light_positions, light_colors, focal_length, field_of_view, camera_position);
         std::cout << "\033[H\033[J"; // Clear the screen
@@ -80,6 +90,7 @@ int main(int argc, char* argv[])
 {
     warmup_cuda();
     setup_scene(argc, argv);
+    mesh_count = argc - 1;
     if(animate){
         animate_main();
         return 0;
